c432a81f8ed1b08b4bd5669a1c6f7ffaa42dacdf
silviobsjunior <50088404+silviobsjunior@users.noreply.github.com>
2020-05-30 16:37:06 -0300

    Create chatmultiplo.js

---
+++ chatmultiplo.js
@@ -0,0 +1,19 @@
+function() { 
+  alert("A URL desta pagina é: " + window.location.href);
+      
+  var pathArray = window.location.href;
+
+  var pathArray = pathArray.split( '/' );
+
+  var sellerer = pathArray[3];
+
+  if (sellerer === 'shopinconfidentes') {
+    window.open('https://www.instagram.com/shopinconfidentes/?hl=pt-br');
+  } else if (sellerer === 'teste') {
+    window.open('https://www.instagram.com/benzadeusloja/?hl=pt-br');
+  } else if (sellerer === 'duopassos') {
+    window.open('https://www.instagram.com/sapatilhasduopassos/?hl=pt-br');
+  } else {
+    window.open('https://pt.khanacademy.org/login');
+  }
+}
